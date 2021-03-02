4967d50aeb60cc63de448a4f13f86274b14b57ad
백승열 <beck33333@naver.com>
2021-03-02 12:20:42 +0900

    Update Item19

---
+++ src/me/prac/Book/Item19/Sub.java
@@ -0,0 +1,22 @@
+package me.prac.Book.Item19;
+
+import java.time.Instant;
+
+public class Sub extends Super{
+
+    private final Instant instant;
+
+    public Sub() {
+        instant = Instant.now();
+    }
+
+    @Override
+    public void overrideMe() {
+        System.out.println(instant);
+    }
+
+    public static void main(String[] args) {
+        Sub sub = new Sub();
+        sub.overrideMe();
+    }
+}
---
+++ src/me/prac/Book/Item19/Super.java
@@ -0,0 +1,11 @@
+package me.prac.Book.Item19;
+
+public class Super {
+
+    public Super() {
+        overrideMe();
+    }
+
+    public void overrideMe() {
+    }
+}
